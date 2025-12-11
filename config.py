06e146c5352c24dfadd1273f2bbbdd56ca1a1323
Florian PEREIRA <florian.pereira@edu.esiee.fr>
2025-12-11 19:36:20 +0100

    Ajout de la méthode get_live_traffic_data pour recup les infos sur les avions dans les airs

--- config.py
+++ config.py
@@ -13,13 +13,18 @@ CLEANED_DIR = os.path.join(DATA_DIR, "cleaned")
 # API Dynamique (Avions)
 OPENSKY_URL = "https://opensky-network.org/api/states/all"
 
-# Fichier Statique (Aéroports du monde - Source: OurAirports)
-AIRPORTS_URL = "https://davidmegginson.github.io/ourairports-data/airports.csv"
+# B. Source Statique (Infrastructure Aérienne - OpenFlights)
+# Documentation : https://openflights.org/data
+# Note : On utilise les fichiers bruts (Raw) depuis GitHub
+AIRPORTS_URL = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+ROUTES_URL = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat"
 
 # --- 3. Noms des Fichiers (Stockage) ---
 # Fichiers Bruts (Raw)
 TRAFFIC_RAW_FILE = os.path.join(RAW_DIR, "traffic_raw.csv")
 AIRPORTS_RAW_FILE = os.path.join(RAW_DIR, "airports_raw.csv")
+ROUTES_RAW_FILE = os.path.join(RAW_DIR, "routes_raw.csv")
 
 # Fichiers Nettoyés (Cleaned) - Prêts pour le dashboard
 AIRPORTS_CLEANED_FILE = os.path.join(CLEANED_DIR, "airports_cleaned.csv")
+TRAFFIC_CLEANED_FILE = os.path.join(CLEANED_DIR, "traffic_processed.csv")
--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -1,2 +1,67 @@
-def get_live_traffic():
-def get_airports():
+import urllib.request
+import urllib.error
+import json
+import pandas as pd
+import os
+import sys
+
+# -----------------------------------------------------------
+# 1. HACK D'IMPORTATION
+# Permet de remonter à la racine pour trouver 'config.py'
+# -----------------------------------------------------------
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from config import (
+    OPENSKY_URL, TRAFFIC_RAW_FILE, 
+    AIRPORTS_RAW_FILE,  AIRPORTS_URL,
+    ROUTES_RAW_FILE, ROUTES_URL)
+
+# -----------------------------------------------------------
+# 2. FONCTIONS DE RÉCUPÉRATION DE DONNÉES
+# -----------------------------------------------------------
+
+def get_live_traffic_data():
+    """
+    Récupère les avions en temps réel via l'API OpenSky.
+    Sauvegarde les données brutes dans 'traffic_raw.csv'.
+    """
+    print("[1/2] Connexion à OpenSky (Trafic en temps réel)...")
+    
+    # Paramètres pour cibler la l'Europe
+    # l'API OpenSky demande des paramètres dans l'URL
+    params = "?lamin=35.00&lomin=-15.00&lamax=72.00&lomax=45.00"
+    url = OPENSKY_URL + params
+    
+    try:
+
+        with urllib.request.urlopen(url) as response:
+            
+            data= response.read()
+            data_str = data.decode('utf-8')
+            json_data = json.loads(data_str)
+            
+            # Extraction de la liste des avions
+            flights = json_data.get('states', [])
+            
+            if flights:
+
+                cols = ["icao24", "callsign", "origin_country", "time_position", 
+                        "last_contact", "longitude", "latitude", "baro_altitude", 
+                        "on_ground", "velocity", "true_track", "vertical_rate", 
+                        "sensors", "geo_altitude", "squawk", "spi", "position_source"]
+                
+  
+                df = pd.DataFrame(flights, columns=cols)
+                df.to_csv(TRAFFIC_RAW_FILE, index=False)
+                print(f"  SUCCÈS : {len(df)} avions récupérés et sauvegardés dans 'traffic_raw.csv'.")
+            else:
+                print(" Aucun avion trouvé (Vérifie ta connexion ou l'heure).")
+
+    except urllib.error.URLError as e:
+        print(f"  Erreur de connexion Internet : {e}")
+    except Exception as e:
+        print(f"  Erreur inattendue : {e}")
+
+#Test de la fonction
+if __name__ == "__main__":
+    get_live_traffic_data()
