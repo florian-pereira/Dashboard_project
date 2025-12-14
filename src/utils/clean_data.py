39aae64f9f8d1383435cc9a1b3c640f060231916
Florian Pereira <florian.pereira@edu.esiee.fr>
2025-12-14 22:40:11 +0100

    get_data et clean_data fonctionel, les données dont on a besoin pour la suite se trouve dans data/cleaned dans config.py on retrouve les variables a utilisées pour recup les csv

---
+++ src/utils/clean_data.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import os
+import sys
+
+# Permet de trouver le fichier config.py à la racine du projet
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from config import (
+    TRAFFIC_RAW_FILE, AIRPORTS_RAW_FILE, ROUTES_RAW_FILE,
+    TRAFFIC_CLEANED_FILE, AIRPORTS_CLEANED_FILE
+)
+
+def process_live_traffic():
+    """
+    Nettoie les données de trafic  (Avions).
+    """
+    print("Nettoyage du Trafic Aérien...")
+    
+    if not os.path.exists(TRAFFIC_RAW_FILE):
+        print(" Fichier introuvable. Lancer d'abord get_data.py.")
+        return
+
+    try:
+        df = pd.read_csv(TRAFFIC_RAW_FILE)
+
+        # On ne garde que les avions EN L'AIR
+        df = df[ (df['on_ground'] == False) & (df['baro_altitude'].notna()) ].copy()
+
+        #  Vitesse m/s en km/h
+        if 'velocity' in df.columns:
+            df['velocity_kmh'] = df['velocity'] * 3.6
+        else:
+            df['velocity_kmh'] = 0
+
+        # Création de la colonne 'origin_category' 
+        # qui nous servira pour donner la couleur entre les avions Francais et internationaux.    
+        # fonction de tri des pays (France vs International)
+        def trier_pays(x):
+            if x == 'France':
+                return 'France'     # On garde France
+            else:
+                return 'International' # Tout le reste devient International
+
+        df['origin_category'] = df['origin_country'].apply(trier_pays)
+        
+
+        #  On ne garde que les colonnes utiles :
+        #
+        
+        cols_to_keep = [
+            'icao24', 'callsign', 'origin_country', 'longitude', 'latitude', 
+            'baro_altitude', 'true_track', 'velocity_kmh', 'origin_category'
+        ]
+        cols_final = [c for c in cols_to_keep if c in df.columns]
+        df = df[cols_final]
+
+        df.to_csv(TRAFFIC_CLEANED_FILE, index=False)
+        print(f" Il y a {len(df)} avions en vol.")
+
+    except Exception as e:
+        print(f"  Erreur nettoyage trafic : {e}")
+
+def process_static_data():
+    """
+    Fusionne Aéroports et Routes pour calculer la taille des hubs.
+    """
+    print(" Fusion Aéroports & Routes...")
+    
+    if not os.path.exists(AIRPORTS_RAW_FILE) or not os.path.exists(ROUTES_RAW_FILE):
+        print(" Fichiers RAW introuvables (Airports ou Routes manquants).")
+        return
+
+    try:
+        
+        df_air = pd.read_csv(AIRPORTS_RAW_FILE)
+        df_routes = pd.read_csv(ROUTES_RAW_FILE)
+
+        # Nettoyage de base avec dropna() on ne garde que les aéroports avec Latitude, Longitude et IATA valides
+        df_air = df_air.dropna(subset=['Latitude', 'Longitude', 'IATA'])
+        
+        # Compté le nombre de routes par aéroport
+        route_counts = df_routes['Source Airport'].value_counts().reset_index() #reset_index pour transformer en une nouvelle DataFrame
+        route_counts.columns = ['IATA', 'Route_Count']
+
+        # fusion des données d'aéroports avec le nombre de routes dans une nouvelle dataframe
+        df_merged = pd.merge(df_air, route_counts, on='IATA', how='left')
+        df_merged['Route_Count'] = df_merged['Route_Count'].fillna(0) # Remplacer NaN par 0
+
+        # on garde les aéroports avec au moins 5 routes 
+        df_final = df_merged.query("Route_Count >= 5 ").copy()
+
+        # Sauvegarde
+        df_final.to_csv(AIRPORTS_CLEANED_FILE, index=False)
+        print(f" Aéroports traités : {len(df_final)} lignes.")
+
+    except Exception as e:
+        print(f" Erreur traitement aéroports : {e}")
+
+if __name__ == "__main__":
+    process_live_traffic()
+    process_static_data()
--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -5,9 +5,8 @@ import pandas as pd
 import os
 import sys
 
-# -----------------------------------------------------------
 # Permet de remonter à la racine pour trouver 'config.py'
-# -----------------------------------------------------------
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from config import (
@@ -15,16 +14,15 @@ from config import (
     AIRPORTS_RAW_FILE,  AIRPORTS_URL,
     ROUTES_RAW_FILE, ROUTES_URL)
 
-# -----------------------------------------------------------
-# 2. FONCTIONS DE RÉCUPÉRATION DE DONNÉES
-# -----------------------------------------------------------
+
+# FONCTIONS DE RÉCUPÉRATION DE DONNÉES
 
 def get_live_traffic_data():
     """
     Récupère les avions en temps réel via l'API OpenSky.
     Sauvegarde les données brutes dans 'traffic_raw.csv'.
     """
-    print("[1/2] Connexion à OpenSky pour récupérer les données de trafic aérien...")
+    print("[Connexion à OpenSky pour récupérer les données de trafic aérien...")
     
     # Paramètres pour cibler la l'Europe
     # l'API OpenSky demande des paramètres dans l'URL
@@ -43,7 +41,7 @@ def get_live_traffic_data():
             flights = json_data.get('states', [])
             
             if flights:
-
+                # On définit les colonnes selon la doc OpenSky: https://openskynetwork.github.io/opensky-api/rest.html
                 cols = ["icao24", "callsign", "origin_country", "time_position", 
                         "last_contact", "longitude", "latitude", "baro_altitude", 
                         "on_ground", "velocity", "true_track", "vertical_rate", 
@@ -52,7 +50,7 @@ def get_live_traffic_data():
   
                 df = pd.DataFrame(flights, columns=cols)
                 df.to_csv(TRAFFIC_RAW_FILE, index=False)
-                print(f"  SUCCÈS : {len(df)} avions récupérés et sauvegardés dans 'traffic_raw.csv'.")
+                print(f" {len(df)} avions récupérés .")
             else:
                 print(" Aucun avion trouvé (Vérifie ta connexion ou l'heure).")
 
@@ -61,6 +59,35 @@ def get_live_traffic_data():
     except Exception as e:
         print(f"  Erreur inattendue : {e}")
 
+def get_static_AeroportsRoads_data():
+
+    print(" Téléchargement OpenFlights...")
+    try:
+        #Documentation : https://openflights.org/data pour les noms des colonnes (aireports et routes)
+        # ---  AÉROPORTS ---
+        
+        cols_airports = ["Airport ID", "Name", "City", "Country", "IATA", "ICAO", 
+                         "Latitude", "Longitude", "Altitude", "Timezone", "DST", 
+                         "Tz", "Type", "Source"]
+        df_air = pd.read_csv(AIRPORTS_URL, names=cols_airports, header=None)
+        df_air.to_csv(AIRPORTS_RAW_FILE, index=False)
+        print(f" Aéroports : {len(df_air)} lignes.")
+
+        # ---  ROUTES ---
+        cols_routes = ["Airline", "Airline ID", "Source Airport", "Source Airport ID", 
+                       "Dest Airport", "Dest Airport ID", "Codeshare", "Stops", "Equipment"]
+        
+        # On télécharge
+        df_routes = pd.read_csv(ROUTES_URL, names=cols_routes, header=None)
+        
+        # On sauvegarde
+        df_routes.to_csv(ROUTES_RAW_FILE, index=False)
+        print(f" Routes : {len(df_routes)} lignes.")
+        
+    except Exception as e:
+        print(f" Erreur statique : {e}")
+
 #Test de la fonction
 if __name__ == "__main__":
     get_live_traffic_data()
+    get_static_AeroportsRoads_data()
