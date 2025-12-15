a2c525823fd613a6e8d1a8b419de0f515464f804
theo.petreco <theo.petreco@edu.esiee.fr>
2025-12-15 22:41:33 +0100

    Clean up comments in charts.py
    Removed French comments and cleaned up code.

--- src/components/charts.py
+++ src/components/charts.py
@@ -2,12 +2,9 @@ import pandas as pd
 import plotly.express as px
 import os 
 
-# Assurez-vous que le chemin vers le fichier est correct
 file_path = 'airports_cleaned.csv' 
 df = pd.read_csv(file_path)
 
-# Création de l'histogramme de l'altitude
-# J'ajoute nbins pour avoir un peu plus de détail dans la distribution
 fig = px.histogram(df, 
                    x='Altitude', 
                    nbins=50, 
