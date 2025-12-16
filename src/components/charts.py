69468f4c49ce33e116f217ffed45ce29064e5c01
theo.petreco <theo.petreco@edu.esiee.fr>
2025-12-16 15:52:17 +0100

    Fix import statement for AIRPORTS_CLEANED_FILE

--- src/components/charts.py
+++ src/components/charts.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import os 
-from import AIRPORTS_CLEANED_FILE
+from config import AIRPORTS_CLEANED_FILE
 file_path = 'airports_cleaned.csv' 
 df = pd.read_csv(file_path)
 
