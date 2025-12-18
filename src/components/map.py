91577422bb864de449b5a5b57e8f07c43ff738c4
unknown <killian.mauge@edu.esiee.fr>
2025-12-18 19:43:37 +0100

    Ajout rotation avions et icones

--- src/components/map.py
+++ src/components/map.py
@@ -6,62 +6,69 @@ import math
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from config import (
-    AIRPORTS_CLEANED_FILE,
-    TRAFFIC_CLEANED_FILE
-    )
+from src.utils.clean_data import load_data
 
-#Lecture du fichier csv dans une dataframe
-df_airports = pd.read_csv(AIRPORTS_CLEANED_FILE)
+def create_map_airports_plane():
 
-LATS_PORTS = df_airports["Latitude"]
+    #Lecture du fichier csv dans une dataframe
+    df_airports = load_data("airports")
 
-LONGS_PORTS = df_airports["Longitude"]
+    LATS_PORTS = df_airports["Latitude"]
 
-ROUTES = df_airports["Route_Count"]
+    LONGS_PORTS = df_airports["Longitude"]
 
-NAME_PORTS = df_airports["Name"]
+    ROUTES = df_airports["Route_Count"]
 
-coords = (46.539758, 2.430331)
-map = folium.Map(location=coords, tiles='OpenStreetMap', zoom_start=6)
+    NAME_PORTS = df_airports["Name"]
 
-fg_airports = folium.FeatureGroup(name="Aéroports")
+    coords = (46.539758, 2.430331)
+    map = folium.Map(location=coords, tiles='cartodb positron', zoom_start=6)
 
-for lat,lng,size,name in zip(LATS_PORTS,LONGS_PORTS,ROUTES,NAME_PORTS):
-    folium.CircleMarker(
-        location = [lat,lng],
-        radius = math.sqrt(size)*0.5,
-        color = 'crimson',
-        fill = True,
-        fill_color = 'crimson',
-        tooltip = name,
-        popup = "{} routes".format(size)
-    ).add_to(fg_airports)
+    fg_airports = folium.FeatureGroup(name="Aéroports")
 
-fg_airports.add_to(map)
+    for lat,lng,size,name in zip(LATS_PORTS,LONGS_PORTS,ROUTES,NAME_PORTS):
+        folium.CircleMarker(
+            location = [lat,lng],
+            radius = math.sqrt(size)*0.5,
+            color = 'crimson',
+            fill = True,
+            fill_color = 'crimson',
+            tooltip = name,
+            popup = "{} routes".format(size)
+        ).add_to(fg_airports)
 
-#Avions
+    fg_airports.add_to(map)
 
-df_plane = pd.read_csv(TRAFFIC_CLEANED_FILE)
+    #Avions
 
-LATS_PLANE = df_plane["latitude"]
+    df_plane = load_data("traffic")
 
-LONGS_PLANE = df_plane["longitude"]
+    LATS_PLANE = df_plane["latitude"]
 
-fg_planes = folium.FeatureGroup(name="Avions en vol")
+    LONGS_PLANE = df_plane["longitude"]
 
-for lat,lng in zip(LATS_PLANE,LONGS_PLANE):
-    icone_avion = folium.Icon(color='red', icon_color='white', icon='plane', prefix='fa')
-    folium.Marker(
-        location = [lat,lng],
-        icon = icone_avion,
-    ).add_to(fg_planes)
+    ANGLE_AVION = df_plane["true_track"]
 
-fg_planes.add_to(map)
+    fg_planes = folium.FeatureGroup(name="Avions en vol")
 
-folium.LayerControl().add_to(map)
+    for lat,lng,angle in zip(LATS_PLANE,LONGS_PLANE,ANGLE_AVION):
+        content_html = f"""
+            <div style="transform: rotate({angle}deg);">
+                <img src="avion_icone.png" style="width:30px; height:30px;">
+            </div>
+        """
+        icone_avion = folium.DivIcon(
+            icon_size = (35,35),
+            icon_anchor = (15,15),
+            html = content_html,
+        )
+        folium.Marker(
+            location = [lat,lng],
+            icon = icone_avion,
+        ).add_to(fg_planes)
 
-map.save(outfile='aeroports_plane.html')
+    fg_planes.add_to(map)
 
+    folium.LayerControl().add_to(map)
 
-
+    map.save(outfile='aeroports_plane.html')
